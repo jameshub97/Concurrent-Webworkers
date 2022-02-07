6e992c4d4ea73513fb019c762c2c44964e0798df
James Carr <jamesalexcarr@outlook.com>
2022-02-07 15:11:38 +0000

    commit

--- book-api.js
+++ book-api.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
+console.log("welcome");
 // Where we will keep books
 let books = [];
 
@@ -15,8 +16,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/book', (req, res) => {
-    // We will be coding here
+    const book = req.body;
+    console.log(book, "added ");
+    books.push(book);
+    res.send('Book added to the database');
+    res.send("<html> <head>server Response</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>");
 });
+
+let counter = [];
+
+app.get("/", (req, res) => {
+    res.send("<html><head>server Response</head><body><button onclick=myFunction()>Click Me</button></body></html>");
+});
+
 
 app.get('/books', (req, res)=> {
     res.json(books);
