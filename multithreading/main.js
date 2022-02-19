8ad93e95c70bf1ef026f3c845d9e244996a37a3c
James Carr <jamesalexcarr@outlook.com>
2022-02-19 16:16:52 +0000

    commit

--- multithreading/main.js
+++ multithreading/main.js
@@ -1,25 +1,51 @@
 const first = document.querySelector('#number1');
 const second = document.querySelector('#number2');
 
-let books = ["Harry Potter", "Percy Jackson", "Dune"];
+function book(name, value){
+    this.name = name
+    this.value = value
+}
+
+
+let book1 = new book("harry potter", "10")
+let book2 = new book("percy jackson", "10")
+let book3 = new book("dune", "10") 
+
+// workers simply randomly choose from an array, this message is sent
+// this
+
+
+
+document.querySelector('.result3').innerHTML = (Object.entries(books));
+
 
 viewbooks = () => {
-    document.querySelector('.result3').innerHTML = books;
+    for(var key in books){
+        var value = books[key]
+        document.querySelector('.result3').innerHTML = value;
+    }
+    
+    if (books == []) {
+        document.querySelector('.result3').innerHTML = "is empty";
+    }
 }
+
 
 startw1 = () => {
     if (window.Worker) {
         const w1 = new Worker("worker.js");
         w1.postMessage(books);
         w1.onmessage = function (event) {
+            viewbooks();
             e1 = event.data.e1;
             e2 = event.data.e2;
             document.querySelector('.result').innerHTML = event.data.e1 + event.data.e2;
             if (books.includes(e1)) {
-                let newArray = books.filter(function(f){return f!== e1})
-                document.querySelector('.result4').innerHTML = newArray;
+                let newArray = books.filter(function (f) { return f !== e1 })
+                document.querySelector('.result4').innerHTML = newArray + " filtered";
                 document.querySelector('.result2').innerHTML = e1 + " is available";
                 books = newArray;
+
             }
             if (e1 == "completed") {
                 startw1();
@@ -40,15 +66,15 @@ startw2 = () => {
         const w2 = new Worker("worker.js");
         w2.postMessage(books);
         w2.onmessage = function (event) {
+            viewbooks();
             e1 = event.data.e1;
             e2 = event.data.e2;
             document.querySelector('.result2').innerHTML = event.data.e1 + event.data.e2;;
             if (books.includes(e1)) {
-                let newArray = books.filter(function(f){return f!== e1})
-                document.querySelector('.result4').innerHTML = newArray;
+                let newArray = books.filter(function (f) { return f !== e1 })
+                document.querySelector('.result4').innerHTML = newArray + " filtered";
                 document.querySelector('.result2').innerHTML = e1 + " is available";
                 books = newArray;
-                
             }
 
             if (e1 == "completed") {
